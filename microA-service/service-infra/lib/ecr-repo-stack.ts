e3887da5b8576a52d042c4b93bc68cc140731ab1
wdai <wenhong.dai@cellsignal.com>
2022-09-27 16:45:08 -0400

    microA EcrRepoStack

--- microA-service/service-infra/lib/ecr-repo-stack.ts
+++ microA-service/service-infra/lib/ecr-repo-stack.ts
@@ -1,13 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdkUtil from './cdkUtil';
+import {RemovalPolicy} from "aws-cdk-lib";
 
 export class EcrRepoStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         new cdk.aws_ecr.Repository(this, cdkUtil.dockerRepoName, {
-            repositoryName: cdkUtil.dockerRepoName
+            repositoryName: cdkUtil.dockerRepoName,
+            removalPolicy: RemovalPolicy.DESTROY
         });
 
     }
